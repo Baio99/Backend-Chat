1255092d286642cf8cfbcf636c78fad461061a45
Bryan Adrian Iza Oña <65301459+Baio99@users.noreply.github.com>
2025-03-27 19:08:37 -0500

    Añadir validación de contraseñas en el modelo de usuario

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+// Hash de la contraseña antes de guardar
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Método para comparar contraseñas
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
